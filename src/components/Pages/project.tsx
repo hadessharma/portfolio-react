03719ced6c59e089e4f7d8dd7205d941b7f91168
Deep Sharma <scsa990@gmail.com>
2024-11-26 20:06:26 -0700

    added projects and hover functionality

--- src/components/Pages/project.tsx
+++ src/components/Pages/project.tsx
@@ -8,6 +8,13 @@ import tictacttoeImage3 from "../../assets/project/tictactoe/3.jpg";
 
 // importing phishing detection
 import phishingdetectionImage1 from "../../assets/project/phishing/1.png";
+import phishingdetectionImage2 from "../../assets/project/phishing/2.png";
+
+// importing terraZure
+import terraZureImage1 from "../../assets/project/terraZure/1.png";
+import terraZureImage2 from "../../assets/project/terraZure/2.png";
+import terraZureImage3 from "../../assets/project/terraZure/3.png";
+import terraZureImage4 from "../../assets/project/terraZure/4.png";
 
 const Project: React.FC = () => {
   return (
@@ -23,10 +30,11 @@ const Project: React.FC = () => {
             </h2>
           </div>
         </div>
-        <div className="flex px-10 space-x-10 sm:flex-col md:flex-row">
+        <div className="flex flex-col sm:flex-row md:space-x-10 md:space-y-0 sm:space-x-5 space-y-5 justify-center items-center px-10">
           {/* card slider */}
           <Card img={tictacttoeImage1} app={true} />
           <Card img={phishingdetectionImage1} app={false} />
+          <Card img={terraZureImage1} app={false} />
         </div>
       </div>
     </>
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -7,34 +7,35 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ img, app }) => {
   return (
     <>
-      <div className="flex border border-black rounded-2xl p-4">
+      <div className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col border border-black rounded-2xl p-4">
         {/* card */}
+
+        <div className="border border-black rounded-2xl">
+          {/* image slider */}
+          <img
+            className={`rounded-2xl ${
+              app ? "sm:max-w-52 md:max-w-72" : "sm:max-w-lg md:max-w-lg"
+            }`}
+            //   "max-w-72 rounded-2xl"
+            src={img}
+            alt="Tic Tac Toe Image 1"
+          />
+        </div>
         <div className="flex flex-col space-y-2">
-          <div className="border border-black rounded-2xl">
-            {/* image slider */}
-            <img
-              className={`rounded-2xl ${app ? "max-w-72" : "max-w-96"}`}
-              //   "max-w-72 rounded-2xl"
-              src={img}
-              alt="Tic Tac Toe Image 1"
-            />
-          </div>
-          <div className="flex flex-col space-y-2">
-            <h3 className="text-center text-2xl font-semibold px-10">
-              Tic Tac Toe
-            </h3>
-            <div className="flex justify-evenly">
-              {/* Buttons */}
-              <div className="border border-black rounded-2xl p-3 min-w-28">
-                <a href="" className="block text-center">
-                  GitHub
-                </a>
-              </div>
-              <div className="border border-black rounded-2xl p-3 min-w-28">
-                <a href="" className="block text-center">
-                  Live Demo
-                </a>
-              </div>
+          <h3 className="text-center text-2xl font-semibold px-10">
+            Tic Tac Toe
+          </h3>
+          <div className="flex justify-evenly">
+            {/* Buttons */}
+            <div className="border border-black rounded-2xl p-3 min-w-28">
+              <a href="" className="block text-center">
+                GitHub
+              </a>
+            </div>
+            <div className="border border-black rounded-2xl p-3 min-w-28">
+              <a href="" className="block text-center">
+                Live Demo
+              </a>
             </div>
           </div>
         </div>
