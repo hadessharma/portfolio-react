2e2020124a6410da1870c69821f7ebff196d4941
Deep Sharma <scsa990@gmail.com>
2025-03-16 19:49:49 -0700

    Moved skills icon evenenly

--- src/components/Cards/experienceCard.tsx
+++ src/components/Cards/experienceCard.tsx
@@ -7,17 +7,17 @@ interface CardProps {
 const ExperienceCard: React.FC<CardProps> = ({ name, logos }) => {
   return (
     <>
-      <div className="flex flex-col md:w-1/5 border-black border my-5 p-2 rounded-2xl">
+      <div className="flex flex-col md:w-1/5 border-black border my-5 p-4 rounded-2xl">
         <div className="flex justify-center text-2xl py-4">
           <h2 className="text-center text-zinc-500 font-semibold">{name}</h2>
         </div>
-        <div className="flex flex-wrap justify-center items-center gap-4 grow mt-2">
+        <div className="flex flex-wrap justify-around items-center gap-4 grow">
           {logos.map((item) => (
             <img
               key={item.title}
               src={item.img}
               alt={item.title}
-              className="max-w-36"
+              className="max-w-28"
             ></img>
           ))}
         </div>
